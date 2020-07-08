5e4178e7816eed66927de655a82e623ed6db424b
Gkoliver <37561701+TrueGkoliver@users.noreply.github.com>
2020-07-08 15:15:23 -0500

    Added some more info

--- src/main/java/main/SecretInfo.java
+++ src/main/java/main/SecretInfo.java
@@ -2,4 +2,9 @@ package main;
 
 public class SecretInfo {
     //Haha, gotcha! You thought that this would be where secret stuff was. It used to be, but now it's not!
+    //If you were considering using the token to attempt and hack, I've got some advise for you.
+    //Sit back, maybe get some water (it's good for you!) and reconsider the life choices that led you down the path
+    //to becoming such a worthless piece of garbage.
+    //Thank you.
+    //-Creator of the bot.
 }
