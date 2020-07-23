7bbc97df80c821619ab518661a3672320b1ebcbe
Gkoliver <37561701+TrueGkoliver@users.noreply.github.com>
2020-07-23 10:54:42 -0500

    A massive amount of new sections

--- src/main/java/game/common/ships/sizes/sections/Sections.java
+++ src/main/java/game/common/ships/sizes/sections/Sections.java
@@ -742,7 +742,7 @@ public class Sections {
 	public static final HashMap<SlotType, Integer> S_COLOSSUS = DoubleSet.generate(
 		make(W, 1)
 	);
-
+	//Here begins Precursor Stuff, from Ancient Cache of Technologies.
 	//Precursor Escort
 	public static final HashMap<SlotType, Integer> S_PCRES1_ESCORT = DoubleSet.generate(
 		make(L, 1)
@@ -918,6 +918,659 @@ public class Sections {
 		make(X, 2),
 		make(T, 1)
 	);
+	public static final HashMap<SlotType, Integer> S_PCHYP1_ALPHA_MISSILE = DoubleSet.generate(
+		make(L, 2),
+		make(G, 4),
+		make(T, 1)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHYP1_ALPHA_CARRIER = DoubleSet.generate(
+		make(H, 7),
+		make(T, 1)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHYP1_OMEGA_CORE = DoubleSet.generate(
+		make(X, 4),
+		make(T, 1)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHYP1_OMEGA_MISSILE = DoubleSet.generate(
+		make(L, 2),
+		make(G, 4),
+		make(X, 2),
+		make(T, 1)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHYP1_OMEGA_CARRIER = DoubleSet.generate(
+		make(H, 7),
+		make(X, 2),
+		make(T, 1)
+	);
+
+	//(x2)
+	public static final HashMap<SlotType, Integer> S_PCHYP2_WING_ARTILLERY = DoubleSet.generate(
+		make(L, 4)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHYP2_WING_MISSILE = DoubleSet.generate(
+		make(G, 8)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHYP2_WING_HANGAR = DoubleSet.generate(
+		make(P, 2),
+		make(H, 6)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHYP2_ALPHA_WING_HANGAR = DoubleSet.generate(
+		make(P, 2),
+		make(H, 8)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHYP2_ALPHA_WING_ARTILLERY = DoubleSet.generate(
+		make(L, 6)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHYP2_ALPHA_WING_MISSILE = DoubleSet.generate(
+		make(L, 6)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHYP2_OMEGA_WING_ARTILLERY = DoubleSet.generate(
+		make(L, 8)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHYP2_OMEGA_WING_MISSILE = DoubleSet.generate(
+		make(L, 4),
+		make(G, 8)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHYP2_OMEGA_WING_HANGAR = DoubleSet.generate(
+		make(P, 2),
+		make(H, 10)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHYP3_HANGAR_MATRIX = DoubleSet.generate(
+		make(H, 4)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHYP3_FIRE_SUPPORT_MATRIX = DoubleSet.generate(
+		make(L, 4)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHYP3_GUIDANCE = DoubleSet.generate(
+		make(G, 8)
+	);
+
+	public static final HashMap<SlotType, Integer> S_PCHYP3_ALPHA_HANGAR_MATRIX = DoubleSet.generate(
+		make(H, 6)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHYP3_ALPHA_FIRE_SUPPORT_MATRIX = DoubleSet.generate(
+		make(L, 6)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHYP3_ALPHA_GUIDANCE_MATRIX = DoubleSet.generate(
+		make(H, 2),
+		make(G, 8)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHYP3_OMEGA_HANGAR_MATRIX = DoubleSet.generate(
+		make(H, 8)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHYP3_OMEGA_FIRE_SUPPORT_MATRIX = DoubleSet.generate(
+		make(L, 8)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHYP3_OMEGA_GUIDANCE_MATRIX = DoubleSet.generate(
+		make(H, 4),
+		make(G, 8)
+	);
+
+	//Precursor Assault Carrier
+	public static final HashMap<SlotType, Integer> S_PAC1_SPINAL = DoubleSet.generate(
+		make(P, 2),
+		make(L, 4),
+		make(X, 1)
+	);
+	public static final HashMap<SlotType, Integer> S_PAC1_FORWARD_ARTILLERY = DoubleSet.generate(
+		make(P, 2),
+		make(L, 6)
+	);
+	public static final HashMap<SlotType, Integer> S_PAC1_ALPHA_SPINAL = DoubleSet.generate(
+		make(P, 2),
+		make(H, 6),
+		make(X, 1)
+	);
+	public static final HashMap<SlotType, Integer> S_PAC1_ALPHA_FORWARD_ARTILLERY = DoubleSet.generate(
+		make(P, 2),
+		make(H, 8)
+	);
+	public static final HashMap<SlotType, Integer> S_PAC1_OMEGA_SPINAL = DoubleSet.generate(
+		make(P, 2),
+		make(L, 4),
+		make(X, 2),
+		make(T, 1)
+	);
+	public static final HashMap<SlotType, Integer> S_PAC1_OMEGA_FORWARD_ARTILLERY = DoubleSet.generate(
+		make(H, 10)
+	);
+
+	//(x3)
+	public static final HashMap<SlotType, Integer> S_PAC2_HANGAR = DoubleSet.generate(
+		make(P, 2),
+		make(H, 6)
+	);
+	public static final HashMap<SlotType, Integer> S_PAC2_ALPHA_HANGAR = DoubleSet.generate(
+		make(P, 2),
+		make(H, 8)
+	);
+	public static final HashMap<SlotType, Integer> S_PAC2_OMEGA_HANGAR = DoubleSet.generate(
+		make(P, 2),
+		make(H, 10)
+	);
+
+	//Precursor Gatekeeper Warden
+	public static final HashMap<SlotType, Integer> S_PCRGKW1_SPINAL = DoubleSet.generate(
+		make(X, 2),
+		make(T, 1)
+	);
+	public static final HashMap<SlotType, Integer> S_PCRGKW1_ALPHA_SPINAL = DoubleSet.generate(
+		make(X, 4),
+		make(T, 1)
+	);
+	public static final HashMap<SlotType, Integer> S_PCRGKW1_SIGMA_SPINAL = DoubleSet.generate(
+		make(X, 2),
+		make(T, 3)
+	);
+	public static final HashMap<SlotType, Integer> S_PCRGKW1_OMEGA_SPINAL = DoubleSet.generate(
+		make(X, 4),
+		make(T, 3)
+	);
+
+	public static final HashMap<SlotType, Integer> S_PCRGKW2_FOCAL_POINT = DoubleSet.generate(
+		make(L, 8)
+	);
+	public static final HashMap<SlotType, Integer> S_PCRGKW2_ALPHA_FOCAL_POINT = DoubleSet.generate(
+		make(L, 12)
+	);
+	public static final HashMap<SlotType, Integer> S_PCRGKW2_OMEGA_FOCAL_POINT = DoubleSet.generate(
+		make(L, 8),
+		make(X, 4)
+	);
+
+	public static final HashMap<SlotType, Integer> S_PCRGKW3_OUTER_CORE = DoubleSet.generate(
+		make(G, 8)
+	);
+	public static final HashMap<SlotType, Integer> S_PCRGKW3_ALPHA_OUTER_CORE = DoubleSet.generate(
+		make(L, 4),
+		make(G, 8)
+	);
+	public static final HashMap<SlotType, Integer> S_PCRGKW3_OMEGA_OUTER_CORE = DoubleSet.generate(
+		make(L, 4),
+		make(G, 8),
+		make(X, 2)
+	);
+
+	public static final HashMap<SlotType, Integer> S_PCRGKW4_INNER_CORE = DoubleSet.generate(
+		make(P, 4),
+		make(H, 4)
+	);
+	public static final HashMap<SlotType, Integer> S_PCRGKW4_ALPHA_INNER_CORE = DoubleSet.generate(
+		make(P, 4),
+		make(H, 6)
+	);
+	public static final HashMap<SlotType, Integer> S_PCRGKW4_OMEGA_INNER_CORE = DoubleSet.generate(
+		make(P, 4),
+		make(H, 10)
+	);
+
+	//Precursor Guardian
+	public static final HashMap<SlotType, Integer> S_PCGA1_ARTILLERY = DoubleSet.generate(
+		make(L, 4)
+	);
+	public static final HashMap<SlotType, Integer> S_PCGA1_MISSILE = DoubleSet.generate(
+		make(L, 2),
+		make(G, 4)
+	);
+	public static final HashMap<SlotType, Integer> S_PCGA1_ALPHA_ARTILLERY = DoubleSet.generate(
+		make(L, 4),
+		make(X, 1)
+	);
+	public static final HashMap<SlotType, Integer> S_PCGA1_ALPHA_MISSILE = DoubleSet.generate(
+		make(L, 2),
+		make(G, 4),
+		make(X, 1)
+	);
+	public static final HashMap<SlotType, Integer> S_PCGA1_OMEGA_MISSILE = DoubleSet.generate(
+		make(L, 6),
+		make(X, 1)
+	);
+	public static final HashMap<SlotType, Integer> S_PCGA1_OMEGA_ARTILLERY = DoubleSet.generate(
+		make(L, 2),
+		make(G, 8),
+		make(X, 1)
+	);
+
+
+	public static final HashMap<SlotType, Integer> S_PCGA2_GUNCANNON = DoubleSet.generate(
+		make(M, 4)
+	);
+	public static final HashMap<SlotType, Integer> S_PCGA2_MISSILE = DoubleSet.generate(
+		make(G, 4)
+	);
+	public static final HashMap<SlotType, Integer> S_PCGA2_ALPHA_GUNCANNON = DoubleSet.generate(
+		make(M, 4),
+		make(L, 2)
+	);
+	public static final HashMap<SlotType, Integer> S_PCGA2_ALPHA_MISSILE = DoubleSet.generate(
+		make(L, 2),
+		make(G, 4)
+	);
+	public static final HashMap<SlotType, Integer> S_PCGA2_OMEGA_GUNCANNON = DoubleSet.generate(
+		make(M, 4),
+		make(L, 4)
+	);
+	public static final HashMap<SlotType, Integer> S_PCGA2_OMEGA_MISSILE = DoubleSet.generate(
+		make(L, 2),
+		make(G, 8)
+	);
+
+	public static final HashMap<SlotType, Integer> S_PCGA3_DEFENSE = DoubleSet.generate(
+		make(P, 4),
+		make(S, 4)
+	);
+	public static final HashMap<SlotType, Integer> S_PCGA3_HANGAR = DoubleSet.generate(
+		make(P, 2),
+		make(H, 2)
+	);
+	public static final HashMap<SlotType, Integer> S_PCGA3_ALPHA_DEFENSE = DoubleSet.generate(
+		make(P, 4),
+		make(M, 4)
+	);
+	public static final HashMap<SlotType, Integer> S_PCGA3_ALPHA_HANGAR = DoubleSet.generate(
+		make(P, 4),
+		make(M, 2),
+		make(H, 2)
+	);
+	public static final HashMap<SlotType, Integer> S_PCGA3_OMEGA_HANGAR = DoubleSet.generate(
+		make(P, 4),
+		make(M, 8)
+	);
+	public static final HashMap<SlotType, Integer> S_PCGA3_OMEGA_DEFENSE = DoubleSet.generate(
+		make(P, 4),
+		make(M, 2),
+		make(H, 4)
+	);
+	//Precursor Sentinel
+	public static final HashMap<SlotType, Integer> S_PCSE1_EYE = DoubleSet.generate(
+		make(T, 1)
+	);
+	public static final HashMap<SlotType, Integer> S_PCSE1_ALPHA_EYE = DoubleSet.generate(
+		make(T, 2)
+	);
+	public static final HashMap<SlotType, Integer> S_PCSE1_OMEGA_EYE = DoubleSet.generate(
+		make(T, 3)
+	);
+	//(x2)
+	public static final HashMap<SlotType, Integer> S_PCSE2_SENTINEL = DoubleSet.generate(
+		make(X, 2)
+	);
+	public static final HashMap<SlotType, Integer> S_PCSE2_ALPHA_SENTINEL = DoubleSet.generate(
+		make(X, 3)
+	);
+	public static final HashMap<SlotType, Integer> S_PCSE2_OMEGA_SENTINEL = DoubleSet.generate(
+		make(H, 2),
+		make(X, 4)
+	);
+
+	//Precursor Instant Pylon
+	public static final HashMap<SlotType, Integer> S_PCIP_DELTA = DoubleSet.generate(
+		make(X, 1)
+	);
+	public static final HashMap<SlotType, Integer> S_PCIP_ALPHA = DoubleSet.generate(
+		make(X, 2)
+	);
+	public static final HashMap<SlotType, Integer> S_PCIP_OMEGA = DoubleSet.generate(
+		make(X, 2),
+		make(T, 1)
+	);
+	//Precursor Instant Repeller
+	//(x2)
+	public static final HashMap<SlotType, Integer> S_PCIR_DELTA = DoubleSet.generate(
+		make(L, 3)
+	);
+	public static final HashMap<SlotType, Integer> S_PCIR_ALPHA = DoubleSet.generate(
+		make(L, 4)
+	);
+	public static final HashMap<SlotType, Integer> S_PCIR_OMEGA = DoubleSet.generate(
+		make(L, 5)
+	);
+
+	//Precursor Herculean
+	public static final HashMap<SlotType, Integer> S_PCHRC1_COLOSSUS = DoubleSet.generate(
+		make(T, 4),
+		make(W, 1)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHRC1_ALPHA_COLOSSUS = DoubleSet.generate(
+		make(T, 10),
+		make(W, 1)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHRC1_SIGMA_COLOSSUS = DoubleSet.generate(
+		make(T, 14),
+		make(W, 1)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHRC1_OMEGA_COLOSSUS = DoubleSet.generate(
+		make(T, 24),
+		make(W, 1)
+	);
+
+	public static final HashMap<SlotType, Integer> S_PCHRC2_TITAN = DoubleSet.generate(
+		make(T, 5)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHRC2_ALPHA_TITAN = DoubleSet.generate(
+		make(T, 10)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHRC2_SIGMA_TITAN = DoubleSet.generate(
+		make(T, 15)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHRC2_OMEGA_TITAN = DoubleSet.generate(
+		make(T, 25)
+	);
+
+	public static final HashMap<SlotType, Integer> S_PCHRC3_GERAS = DoubleSet.generate(
+		make(X, 10)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHRC3_ALPHA_GERAS = DoubleSet.generate(
+		make(X, 15)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHRC3_OMEGA_GERAS = DoubleSet.generate(
+		make(X, 25)
+	);
+
+	public static final HashMap<SlotType, Integer> S_PCHRC3_LETO = DoubleSet.generate(
+		make(L, 15)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHRC3_ALPHA_LETO = DoubleSet.generate(
+		make(L, 20)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHRC3_SIGMA_LETO = DoubleSet.generate(
+		make(L, 25)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHRC3_OMEGA_LETO = DoubleSet.generate(
+		make(L, 30)
+	);
+
+	public static final HashMap<SlotType, Integer> S_PCHRC4_HORATIO = DoubleSet.generate(
+		make(P, 5),
+		make(H, 10)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHRC4_ALPHA_HORATIO = DoubleSet.generate(
+		make(P, 5),
+		make(H, 10),
+		make(G, 5)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHRC4_SIGMA_HORATIO = DoubleSet.generate(
+		make(P, 5),
+		make(H, 15),
+		make(G, 5)
+	);
+	public static final HashMap<SlotType, Integer> S_PCHRC4_OMEGA_HORATIO = DoubleSet.generate(
+		make(P, 5),
+		make(H, 15),
+		make(G, 10)
+	);
+
+	//Precursor Oracle
+	public static final HashMap<SlotType, Integer> S_PCOR1_WING = DoubleSet.generate(
+		make(S, 4)
+	);
+	public static final HashMap<SlotType, Integer> S_PCOR1_ALPHA_WING = DoubleSet.generate(
+		make(S, 4),
+		make(L, 1)
+	);
+	public static final HashMap<SlotType, Integer> S_PCOR1_SIGMA_WING = DoubleSet.generate(
+		make(S, 4),
+		make(L, 2)
+	);
+	public static final HashMap<SlotType, Integer> S_PCOR1_OMEGA_WING = DoubleSet.generate(
+		make(S, 4),
+		make(L, 3)
+	);
+
+	public static final HashMap<SlotType, Integer> S_PCOR2_ANTENNA = DoubleSet.generate(
+		make(P, 4)
+	);
+	public static final HashMap<SlotType, Integer> S_PCOR2_ALPHA_ORACLE = DoubleSet.generate(
+		make(P, 5)
+	);
+	public static final HashMap<SlotType, Integer> S_PCOR2_SIGMA_ORACLE = DoubleSet.generate(
+		make(P, 6)
+	);
+	public static final HashMap<SlotType, Integer> S_PCOR2_OMEGA_ORACLE = DoubleSet.generate(
+		make(P, 7)
+	);
+
+	//Precursor Initializer
+	public static final HashMap<SlotType, Integer> S_PCIN1_RETINA = DoubleSet.generate(
+		make(M, 1)
+	);
+	public static final HashMap<SlotType, Integer> S_PCIN2_CORE = DoubleSet.generate(
+		make(P, 4)
+	);
+	public static final HashMap<SlotType, Integer> S_PCIN3_MAIN = DoubleSet.generate(
+		make(H, 2)
+	);
+
+	//Precursor Emissary
+	public static final HashMap<SlotType, Integer> S_PCEM1_RETINA = DoubleSet.generate(
+		make(L, 1)
+	);
+	public static final HashMap<SlotType, Integer> S_PCEM2_CORE = DoubleSet.generate(
+		make(S, 2)
+	);
+	public static final HashMap<SlotType, Integer> S_PCEM2_SIGMA_CORE = DoubleSet.generate(
+		make(S, 2),
+		make(H, 1)
+	);
+	public static final HashMap<SlotType, Integer> S_PCEM2_OMEGA_CORE = DoubleSet.generate(
+		make(S, 2),
+		make(H, 2)
+	);
+
+	public static final HashMap<SlotType, Integer> S_PCEM3_MAIN = DoubleSet.generate(
+		make(P, 4)
+	);
+
+	//AI Seeker
+	public static final HashMap<SlotType, Integer> S_AISK = DoubleSet.generate(
+		make(M, 3),
+		make(G, 1)
+	);
+	//AI Warform
+	public static final HashMap<SlotType, Integer> S_AIWF = DoubleSet.generate(
+		make(P, 1),
+		make(M, 3),
+		make(L, 2),
+		make(X, 1)
+	);
+
+	//AI Defensive
+	public static final HashMap<SlotType, Integer> S_AIDF = DoubleSet.generate(
+		make(P, 1),
+		make(S, 2),
+		make(M, 2)
+	);
+
+	//AI Nexus
+	public static final HashMap<SlotType, Integer> S_AINX = DoubleSet.generate(
+		make(S, 4),
+		make(L, 2)
+	);
+
+	//AI Core
+	public static final HashMap<SlotType, Integer> S_AICR = DoubleSet.generate(
+		make(P, 2),
+		make(M, 8),
+		make(H, 14),
+		make(X, 2)
+	);
+	//Extradimensional Escort
+	public static final HashMap<SlotType, Integer> S_EXET = DoubleSet.generate(
+		make(M, 3)
+	);
+	//Extradimensional Cruiser
+	public static final HashMap<SlotType, Integer> S_EXCR = DoubleSet.generate(
+		make(S, 2),
+		make(M, 3),
+		make(L, 1)
+	);
+	//Extradimensional Battleship
+	public static final HashMap<SlotType, Integer> S_EXBT = DoubleSet.generate(
+		make(S, 2),
+		make(M, 4),
+		make(L, 3)
+	);
+
+	//Extradimensional Conduit
+	public static final HashMap<SlotType, Integer> S_EXCD = DoubleSet.generate(
+		make(M, 4)
+	);
+	//Extradimensional Junction
+	public static final HashMap<SlotType, Integer> S_EXJC = DoubleSet.generate(
+		make(M, 4),
+		make(L, 4)
+	);
+	//Extradimensional Anchor
+	public static final HashMap<SlotType, Integer> S_EXAN = DoubleSet.generate(
+		make(S, 6),
+		make(M, 6),
+		make(L, 6)
+	);
+
+	//Fallen Empire Escort
+	public static final HashMap<SlotType, Integer> S_FEES = DoubleSet.generate(
+		make(P, 4),
+		make(S, 4),
+		make(L, 1)
+	);
+	public static final HashMap<SlotType, Integer> S_FEBC = DoubleSet.generate(
+		make(M, 4),
+		make(H, 4),
+		make(X, 1)
+	);
+	//Fallen Empire Titan
+	public static final HashMap<SlotType, Integer> S_FETI = DoubleSet.generate(
+		make(M, 4),
+		make(H, 12),
+		make(T, 1)
+	);
+	//Fallen Empire Outpost
+	public static final HashMap<SlotType, Integer> S_FEOP = DoubleSet.generate(
+		make(P, 2),
+		make(M, 5),
+		make(L, 2)
+	);
+	//Fallen Empire Fortress
+	public static final HashMap<SlotType, Integer> S_FEFT = DoubleSet.generate(
+		make(P, 4),
+		make(M, 10),
+		make(L, 10)
+	);
+	//Behemonth Planetcraft
+	//Eraser Planetary Cannon
+	public static final HashMap<SlotType, Integer> S_BHMTH1_ERASER = DoubleSet.generate(
+		make(G, 6),
+		make(T, 6)
+	);
+	//Auxilliary Weapon Battery
+	public static final HashMap<SlotType, Integer> S_BHMTH2_AUXILLIARY = DoubleSet.generate(
+		make(X, 12)
+	);
+	//Weaponized Crust
+	public static final HashMap<SlotType, Integer> S_BHMTH3_CRUST = DoubleSet.generate(
+		make(X, 12)
+	);
+	//Planetary Hangars
+	public static final HashMap<SlotType, Integer> S_BHMTH4_HANGARS = DoubleSet.generate(
+		make(H, 12)
+	);
+	//Secondary Armnament
+	public static final HashMap<SlotType, Integer> S_BHMTH5_SECONDARY = DoubleSet.generate(
+		make(L, 12)
+	);
+
+	//Stellar Systemcraft
+	//Primary Annihalator Lances
+	public static final HashMap<SlotType, Integer> S_STSYS_LANCES = DoubleSet.generate(
+		make(T, 24),
+		make(W, 1)
+	);
+	//Existence Removal Beams
+	public static final HashMap<SlotType, Integer> S_STSYS_EXISTENCE = DoubleSet.generate(
+		make(T, 25)
+	);
+	//Nicoll-Dyson Capacitor Array
+	public static final HashMap<SlotType, Integer> S_STSYS_NICOLL_DYSON = DoubleSet.generate(
+		make(P, 5),
+		make(X, 20)
+	);
+	//Secondary Armnament
+	public static final HashMap<SlotType, Integer> S_STSYS_SECONDARY = DoubleSet.generate(
+		make(P, 5),
+		make(X, 20)
+	);
+	//Grand Stellar Hangars
+	public static final HashMap<SlotType, Integer> S_STSYS_STELLAR = DoubleSet.generate(
+		make(H, 25)
+	);
+	//Omega Kugelblitz Protectors
+	public static final HashMap<SlotType, Integer> S_OMEGA_KUGELBLITZ = DoubleSet.generate(
+		make(L, 25)
+	);
+
+	//Infinity Machine
+	public static final HashMap<SlotType, Integer> S_INFINITY_MACHINE = DoubleSet.generate(
+		make(P, 6),
+		make(S, 5),
+		make(M, 7),
+		make(H, 7),
+		make(X, 1)
+	);
+
+	//Automated Dreadnought
+	public static final HashMap<SlotType, Integer> S_AUTOMATED_DREADNOUGHT = DoubleSet.generate(
+		make(S, 8),
+		make(L, 7)
+	);
+	//Enigmatic Sentinel
+	public static final HashMap<SlotType, Integer> S_ENIGMATIC_SENTINEL = DoubleSet.generate(
+		make(S, 1)
+	);
+	public static final HashMap<SlotType, Integer> S_ENIGMATIC_DEFENDER = DoubleSet.generate(
+		make(S, 2)
+	);
+	public static final HashMap<SlotType, Integer> S_ENIGMATIC_WARDEN = DoubleSet.generate(
+		make(S, 1),
+		make(L, 1)
+	);
+	public static final HashMap<SlotType, Integer> S_ENIGMATIC_VAULT = DoubleSet.generate(
+		make(L, 7)
+	);
+
+	//Reclaimer Bot
+	public static final HashMap<SlotType, Integer> S_RECLAIMER_BOT1 = DoubleSet.generate(
+		make(G, 7),
+		make(X, 1)
+	);
+	//Reclaimer Droid (x3s)
+	public static final HashMap<SlotType, Integer> S_RECLAIMER_BOT2 = DoubleSet.generate(
+		make(G, 1)
+	);
+	//Ether Drake
+	public static final HashMap<SlotType, Integer> S_ETHER_DRAKE = DoubleSet.generate(
+		make(P, 4),
+		make(M, 8),
+		make(X, 1)
+	);
+	//Stellarite Devourer
+	public static final HashMap<SlotType, Integer> S_STELLARITE_DEVOURER = DoubleSet.generate(
+		make(P, 8),
+		make(L, 8),
+		make(X, 1)
+	);
+	//Spectral Wraith
+	public static final HashMap<SlotType, Integer> S_SPECTRAL_WRAITH = DoubleSet.generate(
+		make(L, 6)
+	);
+	//Dimensional Horror
+	public static final HashMap<SlotType, Integer> S_DIMENSIONAL_HORROR = DoubleSet.generate(
+		make(P, 4),
+		make(S, 2),
+		make(M, 2),
+		make(L, 2),
+		make(X, 1)
+	);
+
 
 
 
