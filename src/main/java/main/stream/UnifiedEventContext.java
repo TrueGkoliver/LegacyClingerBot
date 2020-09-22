0a2c89721289911b80e1824577177bee96a2a57f
Gkoliver <37561701+TrueGkoliver@users.noreply.github.com>
2020-09-22 15:18:33 -0500

    Context work

--- src/main/java/main/stream/UnifiedEventContext.java
+++ src/main/java/main/stream/UnifiedEventContext.java
@@ -1,4 +1,12 @@
 package main.stream;
 
+import game.common.government.Empire;
+import game.common.planets.PlanetInstance;
+import game.common.ships.ShipInstance;
+
 public class UnifiedEventContext {
+    //This is still in construction, but it'll hopefully work to provide  a universal standard for all game functions going forward.
+    PlanetInstance planetIn;
+    ShipInstance shipIn;
+    Empire empireIn;
 }
